5234911b8338a8fc4a82c33ed0f40729b50a759c
Adam Byczkowski <adam.m.byczkowski@gmail.com>
2020-11-23 17:45:31 -0600

    Added more stuff

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "supersecret"
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/', methods=["GET", "POST"])
+def landing_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(form.username.data))
+        return redirect("/user_page")
+    return render_template('landing_page.html', form=form)
+
+@app.route("/user_page")
+def user_page():
+    return render_template('user_page.html')
+
+@app.route("/settings")
+def settings():
+    return render_template('construction.html')
+
+@app.route("/friends")
+def friends():
+    return render_template('construction.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
